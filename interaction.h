540ea4e88bc964b9ccce43f96732e2db5e8d1cc1
nhy42 <26034461+nhy42@users.noreply.github.com>
2022-05-15 01:05:29 +0200

    Polish

--- interaction.h
+++ interaction.h
@@ -12,6 +12,13 @@
 void printGrid(SIZEDGRID grid, SIZEDGRID mask, int ignoreMask);
 void pickCoords(int* x, int* y, int size);
 int getSize();
+int getValue(int x, int y);
+SIZEDGRID getMask(int size);
+
+void play();
+void autoSolveInterface();
+void genGridInterface();
+
 int mainMenu();
 
 #endif //TAKUZU_INTERFACE_H
